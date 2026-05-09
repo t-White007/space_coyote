71541d6659314bcdbf975921fbd4cdd71a259db9
Tyler White <tylerwhite@Tylers-MacBook-Pro.local>
2026-05-09 14:49:57 +1200

    add site URL to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  site: 'https://jazzy-sable-0d28c2.netlify.app',
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
